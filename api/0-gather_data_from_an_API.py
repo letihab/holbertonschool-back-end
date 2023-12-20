f57619466a01e4254491c3af78b8addbd4f5379a
letihab <letihabib1822@gmail.com>
2023-12-20 11:16:41 +0100

    update file gather data from an api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,37 +2,37 @@
 """
 Uses the JSON placeholder api to query data about an employee
 """
+from sys import argv
 import requests
-import sys
 
-def gather_data_from_api(user_id):
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = f"{main_url}/user/{user_id}/todos"
-    name_url = f"{main_url}/users/{user_id}"
-    
-    todo_response = requests.get(todo_url)
-    name_response = requests.get(name_url)
-    
-    if todo_response.status_code != 200 or name_response.status_code != 200:
-        print("Failed to fetch data. Please check the user ID.")
-        return
-    
-    todo_result = todo_response.json()
-    name_result = name_response.json()
-    
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result if todo.get("completed")])
-    name = name_result.get("name")
-    
-    print(f"Employee {name} is done with tasks ({todo_complete}/{todo_num}):")
-    
-    for todo in todo_result:
-        if todo.get("completed"):
-            print(f"\t{todo.get('title')}")
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py user_id")
-    else:
-        user_id = sys.argv[1]
-        gather_data_from_api(user_id)
+def get_employee_todos(user_id):
+    """get the reponse and format and print data"""
+    number_completed = 0
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_response = requests.get(user_url)
+    user = user_response.json()
+
+    employee_name = user.get("name")
+
+    for task in todos:
+        if task.get("completed"):
+            number_completed += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_completed, len(todos)))
+    for task in todos:
+        if task.get("completed"):
+            formated_title = "	 " + task.get("title")
+            print("{}".format(formated_title))
+
+
+if __name__ == "__main__":
+    """main function"""
+    get_employee_todos(argv[1])
